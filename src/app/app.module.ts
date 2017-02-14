5f0ba10783e7eabae607f00e2b1c28753235154b
Marvin Zumbado <marvinzzz@gmail.com>
2017-02-14 15:07:45 -0600

    Routing partial changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router'
+
 
 import { AppComponent } from './app.component';
 import { AlertModule } from 'ng2-bootstrap';
@@ -11,17 +13,22 @@ import {ToastModule} from 'ng2-toastr/ng2-toastr';
 
 import { Test1Component } from './test1/test1.component';
 import { Test2Component } from './test2/test2.component';
+import { NavComponent } from './nav/nav.component';
+
+import { appRoutes } from './routes'
 
 @NgModule({
   declarations: [
     AppComponent,
     Test1Component,
-    Test2Component
+    Test2Component,
+    NavComponent
   ],
   imports: [
     AlertModule.forRoot(), 
     ToastModule.forRoot(),
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     FormsModule,
     HttpModule
   ],
